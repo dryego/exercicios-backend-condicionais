43fad5d6f2cdc338be69c66fe16caf36d8feb04c
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-08 09:07:30 -0300

    resolução primeiro exercicio da lista

--- 01/index.js
+++ 01/index.js
@@ -1,4 +1,30 @@
-const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogador1 = "pedra";
+const jogador2 = "tesoura";
 
-//seu código aqui
+if (jogador1 == "pedra") {
+    if (jogador2 == "tesoura") {
+        console.log("Pedra");
+    } else if (jogador2 == "papel") {
+        console.log("papel");
+    } else if (jogador2 == "pedra") {
+        console.log("empate");
+    }
+} else if (jogador1 == "tesoura") {
+    if (jogador2 == "tesoura") {
+        console.log("Empate");
+    } else if (jogador2 == "papel") {
+        console.log("Tesoura");
+    } else if (jogador2 == "pedra") {
+        console.log("Pedra");
+    }
+} else if (jogador1 == "papel") {
+    if (jogador2 == "tesoura") {
+        console.log("Tesoura");
+    } else if (jogador2 == "papel") {
+        console.log("Empate");
+    } else if (jogador2 == "pedra") {
+        console.log("Papel");
+    }
+} else if (jogador1 == "" || jogador2 == "") {
+    console.log("Um dos jogadores esta disarmado.");
+}
