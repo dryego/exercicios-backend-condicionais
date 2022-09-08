ef767c84a29f782dda32c188c9f02dd69aa24493
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-08 20:57:06 -0300

    Resolvido, Posicoes dos jogadores aprovados na peneira

--- 06/index.js
+++ 06/index.js
@@ -1,3 +1,13 @@
-const alturaEmCm = 185;
+const alturaEmCm = 100;
 
-//seu código aqui
+if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+    console.log("**LÍBERO**");
+} else if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+    console.log("**PONTEIRO**");
+} else if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+    console.log("**OPOSTO**");
+} else if (alturaEmCm > 205) {
+    console.log("**CENTRAL**");
+} else {
+    console.log("**REPROVADO**")
+}
