51dee87798698ed4ecc768a2eb38c53e3d8eebbd
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-08 22:09:20 -0300

    Resolução, Montanha russa muito assustadora

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,19 @@
 const idade = 18;
-const possuiPatologia = false;
+const possuiPatologia = true;
 const altura = 180;
-const ehEstudante = false;
+const ehEstudante = true;
+
+if (idade < 18 && idade >= 65 && possuiPatologia === true && altura <= 150) {
+    console.log("Você esta BARRADA");
+} else if (idade >= 18) {
+    if (possuiPatologia === true || altura <= 150) {
+        console.log("Você esta BARRADA")
+    } else if (ehEstudante === true) {
+        console.log("Page R$10,00 (Meia Entrada)");
+    } else if (idade >= 65) {
+        console.log("Você esta BARRADA")
+    }
+} else {
+    console.log("Pague R$20,00")
+}
 
