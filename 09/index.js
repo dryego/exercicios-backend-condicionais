285887bce23b4cb249dfa9ccf4fcd89d9df91e40
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-08 22:28:07 -0300

    Resolução, Migração de notas.

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,23 @@
-const nota = 8.5;
+const nota = 11;
+
+if (nota > 10) {
+    console.log("Nota Invalida");
+
+} else if (nota <= 10 && nota >= 9) {
+    console.log("O estudando obteve conceito A");
+
+} else if (nota < 9 && nota >= 8) {
+    console.log("O estudando obteve conceito B");
+
+} else if (nota < 8 && nota >= 6) {
+    console.log("O estudando obteve conceito C");
+
+} else if (nota < 6 && nota >= 4) {
+    console.log("O estudando obteve conceito D");
+
+} else if (nota < 4 && nota >= 0) {
+    console.log("O estudando obteve conceito E");
+
+} else {
+    console.log("Nota Invalida");
+}
