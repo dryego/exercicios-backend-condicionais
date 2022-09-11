c96925d7d39c351711a4be2f99c6948d51027f75
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-10 21:14:33 -0300

    Resolução, utilizado o metodo INCLUDES.

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,17 @@
-const caractere = "E";
+const caractere = "10";
+
+const maiuscula = "A, E, I, O, U";
+const minuscula = "a, e, i, o, u";
+
+if (maiuscula.includes(caractere)) {
+    console.log("Vogal Maiúscula.");
+
+} else if (minuscula.includes(caractere)) {
+    console.log("Vogal Minúscula.");
+
+} else if (caractere >= 0 || caractere <= 9) {
+    console.log("caracter digitado é um numero.");
+
+} else {
+    console.log("caracter é um Vogal.")
+}
