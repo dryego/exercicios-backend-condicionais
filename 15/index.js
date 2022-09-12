7df541112b8423f792c5df9ff2bdd96ab867c993
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 09:08:14 -0300

    Resolução, variação de nomes.

--- 15/index.js
+++ 15/index.js
@@ -1,4 +1,18 @@
-const primeiroNome = "Mario";
-const sobrenome = "";
+const primeiroNome = "";
+const sobrenome = "broz";
 const apelido = "";
 
+if (primeiroNome != false) {
+    if (primeiroNome != false && apelido != false) {
+        console.log(apelido);
+
+    } else if (primeiroNome != false && sobrenome != false) {
+        console.log(primeiroNome, sobrenome);
+
+    } else {
+        console.log(primeiroNome)
+
+    }
+} else {
+    console.log("Declare o primeiro nome.")
+}
