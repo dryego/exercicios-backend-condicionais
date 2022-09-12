1e00a9e1c0edf29ac6155f6f8e136a75d1bee63d
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 10:12:48 -0300

    Quastão INCOMPLETA

--- 17/index.js
+++ 17/index.js
@@ -2,7 +2,12 @@
 const valorDoProduto = 100000;
 
 //quantidade de parcelas
-const quantidadeDoParcelamento = 10;
+const quantidadeDoParcelamento = 5;
 
 //valor pago
-const valorPago = 300;
+const valorPago = 60000;
+
+const valorRestante = valorDoProduto - valorPago;
+const parcelasRestantes = (valorRestante / 1000) - quantidadeDoParcelamento;
+
+console.log(valorRestante, parcelasRestantes);
