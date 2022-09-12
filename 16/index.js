0e360e9244df91f62714ebfcc6812b1f6dd3b18e
dryego barbosa <dryego.lisboa@gmail.com>
2022-09-12 09:24:20 -0300

    Resolução, Matricula de alunos.

--- 16/index.js
+++ 16/index.js
@@ -1,5 +1,21 @@
 //Só é considerado de maior se a idade for maior ou igual a 18.
-const idadeDoAluno = 16;
+const idadeDoAluno = 18;
 
 //valor verdadeiro ou falso que informa se possui responsável
 const possuiResponsavel = false;
+
+if (idadeDoAluno >= 0 && idadeDoAluno < 18) {
+    if (possuiResponsavel != false) {
+        console.log("Rematrícula realizada com sucesso");
+
+    } else {
+        console.log("Não é possível fazer a rematrícula");
+
+    }
+
+} else if (idadeDoAluno >= 18) {
+    console.log("Rematrícula realizada com sucesso");
+
+} else {
+    console.log("Declare a idade do aluno.");
+}
